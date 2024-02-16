89fdb7669a7ace0dc07761ea3f72eac6472ba2fa
jameymyates <jameymyates@gmail.com>
2024-02-16 17:55:02 -0600

    Refactor global error page

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,12 +1,18 @@
 "use client";
 
-function GlobalError({
-  error,
-  reset,
-}: {
+import { FC, useEffect } from "react";
+
+interface GlobalErrorProps {
   error: Error & { digest?: string };
   reset: () => void;
-}) {
+}
+
+const GlobalError: FC<GlobalErrorProps> = ({ error, reset }) => {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
   return (
     <html>
       <body>
@@ -15,6 +21,6 @@ function GlobalError({
       </body>
     </html>
   );
-}
+};
 
 export default GlobalError;
