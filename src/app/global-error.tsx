b45c949431a4502a9b066023a3a7af118b0682a6
jameymyates <jameymyates@gmail.com>
2024-02-16 18:17:54 -0600

    Remove changes from last commit. NextJS already handles production vs development.

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -2,8 +2,6 @@
 
 import { FC, useEffect } from "react";
 
-const env = process.env.NODE_ENV;
-
 interface GlobalErrorProps {
   error: Error & { digest?: string };
   reset: () => void;
@@ -12,11 +10,9 @@ interface GlobalErrorProps {
 const GlobalError: FC<GlobalErrorProps> = ({ error, reset }) => {
   useEffect(() => {
     /**
-     * In production, log the error to an error reporting service
+     * Log the error to an error reporting service
      */
-    if (env == "production") {
-      console.error(error);
-    }
+    console.error(error);
   }, [error]);
 
   return (
@@ -24,12 +20,6 @@ const GlobalError: FC<GlobalErrorProps> = ({ error, reset }) => {
       <body>
         <div role="alert">
           <h2>Something went wrong!</h2>
-          {/**
-           * In development, render error message to screen
-           */}
-          {env == "development" && (
-            <pre style={{ color: "red" }}>{error.message}</pre>
-          )}
           <button onClick={() => reset()}>Try again</button>
         </div>
       </body>
