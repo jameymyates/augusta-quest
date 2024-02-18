cf935cb1222d3195181be02c02e5329619bc7a72
jameymyates <jameymyates@gmail.com>
2024-02-18 13:32:33 -0600

    Install Storybook, addons, and theme

---
+++ .storybook/augusta-quest-theme.tsx
@@ -0,0 +1,74 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+
+  // Typography
+
+  fontBase: "Inter, sans-serif",
+  fontCode: "Inconsolata, monospace",
+
+  brandTitle: "Augusta Quest Storybook",
+  brandUrl: "https://augusta.quest",
+  brandImage: "https://augusta.quest/images/logo-light.png",
+  brandTarget: "_self",
+
+  // Main colors
+
+  /* Picton Blue #45b2e8 */
+  colorPrimary: "hsl(200, 78%, 59%)",
+
+  /* Bahama Blue #106294 */
+  colorSecondary: "hsl(203, 80%, 32%)",
+
+  // UI
+
+  /* Alabaster #f7f7f8 */
+  appBg: "hsl(240, 7%, 97%)",
+
+  /* Alabaster #f7f7f8 */
+  appContentBg: "hsl(240, 7%, 97%)",
+
+  /* Alabaster #f7f7f8 */
+  appPreviewBg: "hsl(240, 7%, 97%)",
+
+  /* Alto #dadadd */
+  appBorderColor: "hsl(240, 4%, 86%)",
+
+  appBorderRadius: 4,
+
+  // Text colors
+
+  /* Woodsmoke #18181a */
+  textColor: "hsl(240, 4%, 10%)",
+
+  /* Alabaster #f7f7f8 */
+  textInverseColor: "hsl(240, 7%, 97%)",
+
+  /* Picton Blue #45b2e8 */
+  textMutedColor: "hsl(200, 78%, 59%)",
+
+  // Toolbar default and active colors
+
+  /* Jumbo #767781 */
+  barTextColor: "hsl(235, 4%, 48%)",
+
+  /* Picton Blue #45b2e8 */
+  barSelectedColor: "hsl(200, 78%, 59%)",
+
+  /* Alabaster #f7f7f8 */
+  barBg: "hsl(240, 7%, 97%)",
+
+  // Form colors
+
+  /* Alabaster #f7f7f8 */
+  inputBg: "hsl(240, 7%, 97%)",
+
+  /* Woodsmoke #18181a */
+  inputBorder: "hsl(240, 4%, 10%)",
+
+  /* Woodsmoke #18181a */
+  inputTextColor: "hsl(240, 4%, 10%)",
+
+  inputBorderRadius: 2,
+});
---
+++ .storybook/manager.tsx
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/manager-api";
+import augustaQuestTheme from "./augusta-quest-theme";
+
+addons.setConfig({
+  theme: augustaQuestTheme,
+});
