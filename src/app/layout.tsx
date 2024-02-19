e34e3736e07f6aacb69c2caaf973b626739d0f05
jameymyates <jameymyates@gmail.com>
2024-02-19 10:55:39 -0600

    Implicity add React import and move theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Script from "next/script";
 import {
   fontDisplay,
@@ -15,31 +16,29 @@ const RootLayout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <Providers>
-      <html lang="en" suppressHydrationWarning>
-        <head />
-        {/**
-         * If you change the Tailwind classes in the body tag, be
-         * sure to update them in ./storybook/preview-body.html as well.
-         *
-         * We could have them automatically applied by putting them in
-         * ./src/styles/globals.css, but I opted to keep them in the jsx
-         * like other styling.
-         */}
-        <body
-          className={cn(
-            "text-base antialiased font-sans font-normal",
-            fontDisplay.variable,
-            fontMono.variable,
-            fontSans.variable,
-            fontSerif.variable
-          )}
-        >
-          {children}
-          <Script src="../node_modules/tw-elements/dist/js/tw-elements.umd.min.js" />
-        </body>
-      </html>
-    </Providers>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      {/**
+       * If you change the Tailwind classes in the body tag, be
+       * sure to update them in ./storybook/preview-body.html as well.
+       *
+       * We could have them automatically applied by putting them in
+       * ./src/styles/globals.css, but I opted to keep them in the jsx
+       * like other styling.
+       */}
+      <body
+        className={cn(
+          "text-base antialiased font-sans font-normal",
+          fontDisplay.variable,
+          fontMono.variable,
+          fontSans.variable,
+          fontSerif.variable
+        )}
+      >
+        <Providers>{children}</Providers>
+        <Script src="../node_modules/tw-elements/dist/js/tw-elements.umd.min.js" />
+      </body>
+    </html>
   );
 };
 
--- src/lib/providers/index.tsx
+++ src/lib/providers/index.tsx
@@ -1,11 +1,18 @@
 "use client";
 
-import { PropsWithChildren } from "react";
+import React from "react";
+import { ThemeProvider } from "next-themes";
 import { Provider } from "react-redux";
 import { reduxStore } from "@/lib/redux";
 
-const Providers = (props: PropsWithChildren) => {
-  return <Provider store={reduxStore}>{props.children}</Provider>;
+const Providers = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+  return (
+    <Provider store={reduxStore}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+      </ThemeProvider>
+    </Provider>
+  );
 };
 
 export { Providers };
