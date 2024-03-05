f84c4d35a68db0ed15360498d5422deb1f625bcc
jameymyates <jameymyates@gmail.com>
2024-03-05 13:34:47 -0600

    Flesh out button component and stories

--- src/features/ui/button/button.stories.tsx
+++ src/features/ui/button/button.stories.tsx
@@ -9,15 +9,155 @@ const meta = {
   },
   tags: ["autodocs"],
   argTypes: {
-    children: {},
+    children: {
+      control: "text",
+      description: "The contents of the button.",
+    },
+    intent: {
+      control: "radio",
+      description: "The style variant of the button.",
+      options: [
+        "default",
+        "primary",
+        "secondary",
+        "tertriary",
+        "success",
+        "warning",
+        "info",
+        "light",
+        "dark",
+        "outline",
+        "floating",
+      ],
+      table: {
+        defaultValue: {
+          summary: "primary",
+        },
+      },
+      title: {
+        control: "text",
+        description:
+          "Used to describe an icon button for screen readers and search engines.",
+      },
+      type: "string",
+    },
+    size: {
+      control: "radio",
+      description: "The size variant of the button.",
+      options: ["small", "medium", "large"],
+      table: {
+        defaultValue: {
+          summary: "medium",
+        },
+      },
+      type: "string",
+    },
   },
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+export const Default: Story = {
+  args: {
+    children: "Default",
+  },
+};
+
 export const Primary: Story = {
   args: {
     children: "Primary",
+    intent: "primary",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    children: "Secondary",
+    intent: "secondary",
+  },
+};
+
+export const Tertriary: Story = {
+  args: {
+    children: "Tertriary",
+    intent: "tertriary",
+  },
+};
+
+export const Success: Story = {
+  args: {
+    children: "Success",
+    intent: "success",
+  },
+};
+
+export const Warning: Story = {
+  args: {
+    children: "Warning",
+    intent: "warning",
+  },
+};
+
+export const Danger: Story = {
+  args: {
+    children: "Danger",
+    intent: "danger",
+  },
+};
+
+export const Info: Story = {
+  args: {
+    children: "Info",
+    intent: "info",
+  },
+};
+
+export const Light: Story = {
+  args: {
+    children: "Light",
+    intent: "light",
+  },
+};
+
+export const Dark: Story = {
+  args: {
+    children: "Dark",
+    intent: "dark",
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    children: "Outline",
+    intent: "outline",
+  },
+};
+
+export const Floating: Story = {
+  args: {
+    children: "Floating",
+    intent: "floating",
+  },
+};
+
+export const Small: Story = {
+  args: {
+    children: "Small",
+    size: "small",
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    children: "Medium",
+    size: "medium",
+  },
+};
+
+export const Large: Story = {
+  args: {
+    children: "Large",
+    size: "large",
   },
 };
--- src/features/ui/button/button.tsx
+++ src/features/ui/button/button.tsx
@@ -1,25 +1,47 @@
-import { ButtonHTMLAttributes, FC, PropsWithChildren } from "react";
+import React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
 
 const button = cva("button", {
   variants: {
     intent: {
-      primary: [
-        "bg-blue-500",
-        "text-white",
-        "border-transparent",
-        "hover:bg-blue-600",
+      primary: ["bg-primary", "text-primary-foreground"],
+      secondary: ["bg-secondary", "text-secondary-foreground"],
+      tertriary: [
+        "text-primary",
+        "shadow-transparent",
+        "hover:shadow-transparent",
+        "focus:shadow-transparent",
+        "active:shadow-transparent",
       ],
-      secondary: [
-        "bg-white",
-        "text-gray-800",
-        "border-gray-400",
-        "hover:bg-gray-100",
+      success: ["bg-success", "text-success-foreground"],
+      warning: ["bg-warning", "text-warning-foreground"],
+      danger: ["bg-danger", "text-danger-foreground"],
+      info: ["bg-info", "text-info-foreground"],
+      light: ["bg-light", "text-dark", "after:from-neutral/30"],
+      dark: ["bg-neutral-950", "text-neutral-50"],
+      outline: [
+        "border-2",
+        "border-primary",
+        "text-primary",
+        "hover:bg-primary/15",
+        "shadow-transparent",
+        "hover:shadow-transparent",
+        "focus:shadow-transparent",
+        "active:shadow-transparent",
+        "after:from-primary/30",
+      ],
+      floating: [
+        "p-2",
+        "rounded-full",
+        "bg-primary",
+        "text-primary-foreground",
       ],
     },
     size: {
-      small: ["text-sm", "py-1", "px-2"],
-      medium: ["text-base", "py-2", "px-4"],
+      small: ["px-4", "pb-[5px]", "pt-[6px]", "text-xs"],
+      medium: ["px-6", "pb-2", "pt-2.5", "text-xs"],
+      large: ["px-7", "pb-2.5", "pt-3", "text-sm"],
     },
   },
   defaultVariants: {
@@ -28,18 +50,56 @@ const button = cva("button", {
   },
 });
 
-const Button: FC<
-  Readonly<
-    ButtonHTMLAttributes<HTMLButtonElement> &
-      VariantProps<typeof button> &
-      PropsWithChildren
-  >
-> = ({ children, className, intent, size }) => {
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof button>,
+    React.PropsWithChildren {
+  ripple?: boolean;
+}
+
+const Button: React.FC<Readonly<ButtonProps>> = ({
+  children,
+  className,
+  intent,
+  ripple = true,
+  size,
+}) => {
+  React.useEffect(() => {
+    if (!ripple) return;
+
+    const init = async () => {
+      const { Ripple, initTE } = await import("tw-elements");
+      initTE({ Ripple });
+    };
+
+    init();
+  }, [ripple]);
+
+  let rippleColor;
+
+  if (intent === "outline") {
+    rippleColor = "primary";
+  } else if (intent === "light") {
+    rippleColor = "#b8b9c1";
+  } else {
+    rippleColor = "light";
+  }
+
   return (
-    <button className={button({ intent, size, className })} type="button">
-      {children}
+    <button
+      className={cn(
+        "inline-block relative overflow-hidden rounded text-base font-medium text-center uppercase leading-normal transition duration-150 ease-in-out focus-visible:ring-2 focus-visible:ring-primary-500 focus-visible:ring-offset-2 focus-visible:ring-offset-white focus-visible:outline-none shadow-[hsla(0,0%,0%,0.2)_0px_3px_1px_-2px,hsla(0,0%,0%,0.14)_0px_2px_2px_0px,hsla(0,0%,0%,0.12)_0px_1px_5px_0px] hover:shadow-[hsla(0,0%,0%,0.2)_0px_2px_4px_-1px,hsla(0,0%,0%,0.14)_0px_4px_5px_0px,hsla(0,0%,0%,0.12)_0px_1px_10px_0px] focus:shadow-[hsla(0,0%,0%,0.2)_0px_3px_1px_-2px,hsla(0,0%,0%,0.14)_0px_2px_2px_0px,hsla(0,0%,0%,0.12)_0px_1px_5px_0px] active:shadow-[hsla(0,0%,0%,0.2)_0px_5px_5px_-3px,hsla(0,0%,0%,0.14)_0px_8px_10px_1px,hsla(0,0%,0%,0.12)_0px_3px_14px_2px] after:bg-gradient-to-r after:from-white/30 after:to-transparent after:h-[155px] after:-left-[75px] after:absolute after:top-[-50px] after:rotate-[35deg] after:transition-all after:duration-[550ms] after:ease-[cubic-bezier(0.19,1,0.22,1)] after:w-[50px] after:z-10 hocus:after:left-[120%] hocus:after:transition-all hocus:after:duration-[550ms] hocus:after:ease-[cubic-bezier(0.19,1,0.22,1)]",
+        button({ className, intent, size })
+      )}
+      data-te-ripple-color={ripple ? rippleColor : undefined}
+      data-te-ripple-init={ripple}
+      type="button"
+    >
+      <span className="z-20">{children}</span>
     </button>
   );
 };
+
+export type { ButtonProps };
 
 export default Button;
