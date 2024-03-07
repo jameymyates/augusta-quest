e0762debf3df95e32a7bd171c9530c183c8dd79e
jameymyates <jameymyates@gmail.com>
2024-03-07 11:56:03 -0600

    Add type to component function

--- src/features/ui/secondary-menu/secondary-menu.tsx
+++ src/features/ui/secondary-menu/secondary-menu.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import Link from "next/link";
 import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Button from "@/features/ui/button";
 
-const SecondaryMenu = () => {
+const SecondaryMenu: React.FC = () => {
   return (
     <div className="inline-flex flex-1 flex-row items-center justify-end gap-x-3 lg:order-2">
       <Button className="hidden lg:inline-flex" intent="link">
