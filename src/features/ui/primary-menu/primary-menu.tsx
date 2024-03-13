dfa11401e55ed4987dfd3e07a3da5ac6daf41cd1
jameymyates <jameymyates@gmail.com>
2024-03-13 13:59:06 -0500

    Add javascript for collapsible menu and fix html typos

--- src/features/ui/primary-menu/primary-menu.tsx
+++ src/features/ui/primary-menu/primary-menu.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 const PrimaryMenu: React.FC = () => {
+  React.useEffect(() => {
+    const init = async () => {
+      const { Collapse, Dropdown, initTE } = await import("tw-elements");
+      initTE({ Collapse, Dropdown });
+    };
+    init();
+  }, []);
+
   return (
     <>
       <button
@@ -19,9 +27,9 @@ const PrimaryMenu: React.FC = () => {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              clip-rule="evenodd"
+              clipRule="evenodd"
               d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-              fill-rule="evenodd"
+              fillRule="evenodd"
             />
           </svg>
         </span>
